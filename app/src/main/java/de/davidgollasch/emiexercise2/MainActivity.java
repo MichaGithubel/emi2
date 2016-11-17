af6b85a8b9985a48d6c6b04e4376aaae849a8008
MichaGithubel <Michael.Scheid@agat.de>
2016-11-17 20:12:48 +0100

    yes

--- app/src/main/java/de/davidgollasch/emiexercise2/MainActivity.java
+++ app/src/main/java/de/davidgollasch/emiexercise2/MainActivity.java
@@ -1,5 +1,6 @@
 package de.davidgollasch.emiexercise2;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,13 +28,6 @@ public class MainActivity extends AppCompatActivity {
     /*
     TODO: (TASK 2.1) Add data structure for contact details here
      */
-    ArrayList<String> title;
-    ArrayList<String> vorname;
-    ArrayList<String> nachname;
-    ArrayList<String> straße;
-    ArrayList<String> stadt;
-    ArrayList<String> land;
-    ArrayList<String> plz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,48 +82,7 @@ public class MainActivity extends AppCompatActivity {
         01067 Dresden           London N6 5PS
         Deutschland             Vereinigtes Königreich
          */
-        title=new ArrayList<>();
-        title.add("Herr");
-        title.add("Frau");
-        title.add("Herr");
-        title.add("Frau");
-        title.add("Frau");
-        vorname=new ArrayList<>();
-        vorname.add("Robert");
-        vorname.add("Elisabeth");
-        vorname.add("Stefan");
-        vorname.add("Ella");
-        vorname.add("Anne");
-        nachname=new ArrayList<>();
-        nachname.add("Meyer");
-        nachname.add("Stramm");
-        nachname.add("Wennige");
-        nachname.add("Beckmann");
-        nachname.add("Watson");
-        straße=new ArrayList<>();
-        straße.add("Apfelbergstraße 10");
-        straße.add("Fritz-Konzert-Straße 1a");
-        straße.add("Kirchplatz 13");
-        straße.add("Falkenstraße 3");
-        straße.add("1 Langdon Park Rd");
-        stadt=new ArrayList<>();
-        stadt.add("St Margrethen");
-        stadt.add("Innsbruck");
-        stadt.add("Wattens");
-        stadt.add("Dresden");
-        stadt.add("London");
-        land=new ArrayList<>();
-        land.add("Schweiz");
-        land.add("Österreich");
-        land.add("Österreich");
-        land.add("Deutschland");
-        land.add("Vereinigtes Königreich");
-        plz=new ArrayList<>();
-        plz.add("9430");
-        plz.add("6020");
-        plz.add("6112");
-        plz.add("01067");
-        plz.add("N6 5PS");
+
         String[] contactName = {"Herr Robert Meyer","Frau Elisabeth Stramm","Herr Stefan Wennige","Frau Ella Beckmann","Frau Anne Watson"};
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, contactName);
         spContacts.setAdapter(adapter);
@@ -162,25 +115,15 @@ public class MainActivity extends AppCompatActivity {
      */
     private void DisplayContactDetails(String contactName) {
         // TODO: Get contact by selected name
-        int id=0;
-        if (contactName=="Herr Robert Meyer")
-            id=0;
-        if (contactName=="Frau Elisabeth Stramm")
-            id=1;
-        if (contactName=="Herr Stefan Wennige")
-            id=2;
-        if (contactName=="Frau Ella Beckmann")
-            id=3;
-        if (contactName=="Frau Anne Watson")
-            id=4;
+
         // TODO: Fill TextViews with contact details
-        tvTitle.setText(land.get(id));
-        tvFirstName.setText(vorname.get(id));
-        tvLastName.setText(nachname.get(id));
-        tvAddress.setText(straße.get(id));
-        tvZip.setText(plz.get(id));
-        tvCity.setText(stadt.get(id));
-        tvCountry.setText(land.get(id));
+        tvTitle.setText("");
+        tvFirstName.setText("");
+        tvLastName.setText("");
+        tvAddress.setText("");
+        tvZip.setText("");
+        tvCity.setText("");
+        tvCountry.setText("");
     }
 
 
